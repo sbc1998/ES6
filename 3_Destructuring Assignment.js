d0073a6691c9b6acb05b92204d1acec8950d8bcb
Shashank B C <bcshashank140@gmail.com>
2024-10-02 15:56:50 +0545

    Create 3_Destructuring Assignment.js
    Destructuring assignment makes it easy to assign array values and object properties to variables.

---
+++ 3_Destructuring Assignment.js
@@ -0,0 +1,18 @@
+
+let artist={
+    name: "Sarah",
+    age: 25,
+    genre: "EDM"
+};
+
+let {name,age,genre}= artist;
+console.log(name);
+console.log(age);
+console.log(genre);
+
+console.log("Array of cars:");
+const car= ["Jeep", "Ford", "Mustang"];
+const[car1,car2,car3]= car;
+console.log(car1);
+console.log(car2);
+console.log(car3);
