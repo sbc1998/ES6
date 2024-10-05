da3eb76c605854d50152a0d2a80b583bfb37fbfb
Shashank B C <bcshashank140@gmail.com>
2024-10-05 23:42:40 +0545

    Create 10_String.startsWith().js
    The startsWith() method returns true if a string begins with specified character(s). If not, it returns false.

---
+++ 10_String.startsWith().js
@@ -0,0 +1,11 @@
+const message = "JavaScript is fun";
+
+// check if message starts with Java
+let result = message.startsWith("Java");
+
+console.log(result);    // true
+
+// check if message starts with Script
+result = message.startsWith("Script");
+
+console.log(result);    // false
