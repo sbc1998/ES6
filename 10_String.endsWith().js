44298a5f3447bd0fbc2c67012d99f28db3af3211
Shashank B C <bcshashank140@gmail.com>
2024-10-05 23:45:55 +0545

    Create 10_String.endsWith().js
    The endsWith() method returns true if a string ends with the specified string. If not, the method returns false.

---
+++ 10_String.endsWith().js
@@ -0,0 +1,8 @@
+// string definition 
+let sentence = "I love electronic dance music";
+
+// checking if the given string ends with "to Carpet."
+let check = sentence.endsWith("music");
+console.log(check);    
+check= sentence.endsWith("usic");
+console.log(check);
