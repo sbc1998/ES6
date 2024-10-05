73bce9552dc01390d5fe44de33a4d17f3ddca64b
Shashank B C <bcshashank140@gmail.com>
2024-10-05 23:39:51 +0545

    Create 9_String.includes().js
    The includes() method checks if one string can be found inside another string.

---
+++ 9_String.includes().js
@@ -0,0 +1,6 @@
+const message = "JavaScript is fun to learn";
+
+// check if message includes the string "Java" and "learn"
+console.log(message.includes("Javascript"));
+console.log(message.includes("learn"));
+
