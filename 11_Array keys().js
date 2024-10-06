c73149f672018895f7b6fd901d8981480ea273bd
Shashank B C <bcshashank140@gmail.com>
2024-10-06 23:38:07 +0545

    Update 11_Array keys().js

--- 11_Array keys().js
+++ 11_Array keys().js
@@ -7,3 +7,13 @@ let iterator = languages.keys();
 for (let key of iterator) {
   console.log(key);
 }
+
+let vehicle = ["car", "bus", , "van", "truck"];
+
+// returns an Array Iterator Object that contains keys
+let iterator = vehicle.keys();
+
+// looping through the iterator object
+for (let key of iterator) {
+  console.log(key);
+}
